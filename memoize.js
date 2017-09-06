0625f54cbedd7f76cbc2451b3be22e177693a70e
Liutong Chen <liutongchen@users.noreply.github.com>
2017-09-05 23:43:48 -0700

    Create memoize.js

---
+++ memoize.js
@@ -0,0 +1,10 @@
+
+const memoize = f => (() => {
+	const cache = new Map();
+	return x => {
+    	if (cache.get(x) == null) {
+        	cache.set(x, f(x));
+        }
+        return cache.get(x);
+    }
+})();
